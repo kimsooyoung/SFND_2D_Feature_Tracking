28a6ad194e3be51474f41faf66f6890a6c3ddb12
kimsooyoung <tge1375@naver.com>
2020-02-24 01:16:16 +0900

    Task7 done.

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -23,6 +23,7 @@ int main(int argc, const char *argv[])
 {
 
     /* INIT VARIABLES AND DATA STRUCTURES */
+    string detectorType = "SIFT"; // SHITOMASI, HARRIS, FAST, BRISK, ORB, AKAZE, SIFT
 
     // data location
     string dataPath = "../";
@@ -74,7 +75,7 @@ int main(int argc, const char *argv[])
 
         // extract 2D keypoints from current image
         vector<cv::KeyPoint> keypoints; // create empty feature list for current image
-        string detectorType = "AKAZE";
+        // string detectorType = "AKAZE";
 
         //// STUDENT ASSIGNMENT
         //// TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
@@ -102,7 +103,7 @@ int main(int argc, const char *argv[])
                 if( !vehicleRect.contains( keypoints[i].pt ) )
                     keypoints.erase(keypoints.begin() + i);
             }
-            // cout << "Keypoints in vehicle Rect n= " << keypoints.size() << endl;
+            cout << "Keypoints in vehicle Rect n= " << keypoints.size() << endl;
         }
         // string windowName = "Debug Window";
         // cv::namedWindow(windowName, 5);
@@ -153,7 +154,7 @@ int main(int argc, const char *argv[])
             /* MATCH KEYPOINT DESCRIPTORS */
 
             vector<cv::DMatch> matches;
-            string matcherType = "MAT_FLANN";        // MAT_BF, MAT_FLANN
+            string matcherType = "MAT_BF";        // MAT_BF, MAT_FLANN
             string descriptorType = "DES_BINARY"; // DES_BINARY, DES_HOG
             string selectorType = "SEL_KNN";       // SEL_NN, SEL_KNN
 
--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -82,6 +82,7 @@ void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool
     double k = 0.04;       // Harris parameter (see equation for details)
 
     // Detect Harris corners and normalize output
+	double t = (double)cv::getTickCount();
     cv::Mat dst, dst_norm, dst_norm_scaled;
     dst = cv::Mat::zeros(img.size(), CV_32FC1);
     cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
@@ -91,42 +92,55 @@ void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool
     // STUDENTS NEET TO ENTER THIS CODE (C3.2 Atom 4)
 
     // Look for prominent corners and instantiate keypoints
-    double maxOverlap = 0.0; // max. permissible overlap between two features in %, used during non-maxima suppression
-    for (size_t j = 0; j < dst_norm.rows; j++)
-    {
-        for (size_t i = 0; i < dst_norm.cols; i++)
+    bool nonMaxima = true;
+    if (nonMaxima){
+        double maxOverlap = 0.0; // max. permissible overlap between two features in %, used during non-maxima suppression
+        for (size_t j = 0; j < dst_norm.rows; j++)
         {
-            int response = (int)dst_norm.at<float>(j, i);
-            if (response > minResponse)
-            { // only store points above a threshold
+            for (size_t i = 0; i < dst_norm.cols; i++)
+            {
+                int response = (int)dst_norm.at<float>(j, i);
+                if (response > minResponse)
+                { // only store points above a threshold
 
-                cv::KeyPoint newKeyPoint;
-                newKeyPoint.pt = cv::Point2f(i, j);
-                newKeyPoint.size = 2 * apertureSize;
-                newKeyPoint.response = response;
+                    cv::KeyPoint newKeyPoint;
+                    newKeyPoint.pt = cv::Point2f(i, j);
+                    newKeyPoint.size = 2 * apertureSize;
+                    newKeyPoint.response = response;
 
-                // perform non-maximum suppression (NMS) in local neighbourhood around new key point
-                bool bOverlap = false;
-                for (auto it = keypoints.begin(); it != keypoints.end(); ++it)
-                {
-                    double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
-                    if (kptOverlap > maxOverlap)
+                    // perform non-maximum suppression (NMS) in local neighbourhood around new key point
+                    bool bOverlap = false;
+                    for (auto it = keypoints.begin(); it != keypoints.end(); ++it)
                     {
-                        bOverlap = true;
-                        if (newKeyPoint.response > (*it).response)
-                        {                      // if overlap is >t AND response is higher for new kpt
-                            *it = newKeyPoint; // replace old key point with new one
-                            break;             // quit loop over keypoints
+                        double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
+                        if (kptOverlap > maxOverlap)
+                        {
+                            bOverlap = true;
+                            if (newKeyPoint.response > (*it).response)
+                            {                      // if overlap is >t AND response is higher for new kpt
+                                *it = newKeyPoint; // replace old key point with new one
+                                break;             // quit loop over keypoints
+                            }
                         }
                     }
+                    if (!bOverlap)
+                    {                                     // only add new key point if no overlap has been found in previous NMS
+                        keypoints.push_back(newKeyPoint); // store new keypoint in dynamic list
+                    }
                 }
-                if (!bOverlap)
-                {                                     // only add new key point if no overlap has been found in previous NMS
-                    keypoints.push_back(newKeyPoint); // store new keypoint in dynamic list
-                }
+            } // eof loop over cols
+        }     // eof loop over rows
+    }else {
+        for (int row = 0; row < dst_norm.rows; row++) {
+            for (int col = 0; col < dst_norm.cols; col++) {
+                cv::KeyPoint point(col, row, 2 * apertureSize, dst_norm.at<float>(row, col));
+                keypoints.push_back(point);
             }
-        } // eof loop over cols
-    }     // eof loop over rows
+        }
+    }
+
+	t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+	cout << "Harris corner detection with n=" << keypoints.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
 
     if (bVis){
         // visualize keypoints
